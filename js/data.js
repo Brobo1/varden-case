73186ada3161957a442c9460b39dd3644713b7f8
Brobo1 <armanahz95@gmail.com>
2024-12-19 00:08:38 +0100

    Add data fetching and integrate local JSON data
    Replaced remote API calls with a local JSON file for data fetching in `getData` function. Integrated the function into `app.js` and updated the HTML to load the script using `defer`. Added sample JSON data file and updated `package.json` with the project name.

--- js/data.js
+++ js/data.js
@@ -1,1 +1,25 @@
+// export function getData() {
+//   const url =
+//     "https://ommu1982.pythonanywhere.com/static/boligprisstatistikk.json";
+//
+//   fetch(url, {
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//   })
+//     .then((res) => {
+//       if (!res.ok) throw new Error("Bad response");
+//       return res.json();
+//     })
+//     .then((data) => console.log(data))
+//     .catch((err) => console.error("Problem with fetch operation", err));
+// }
 
+export async function getData() {
+  await fetch("../data/data.json")
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+    })
+    .catch((err) => console.error("Error fetching JSON:", err));
+}
