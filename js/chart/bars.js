a55c92f4e11d4df7a5a560a02379b0fc163f1cad
brobo1 <armanahz95@gmail.com>
2024-12-20 17:18:40 +0100

    Refactor bar chart spacing logic for consistency.
    Replaces hardcoded spacing and positioning calculations with dynamic logic for improved flexibility. The `addBar` function now takes an additional `spacing` parameter to determine bar placement. This ensures better adaptability to different chart dimensions and data sets.

--- js/chart/bars.js
+++ js/chart/bars.js
@@ -3,34 +3,32 @@ import { getData } from "../data.js";
 const chart = document.getElementById("chart");
 
 let chartStyles = window.getComputedStyle(chart);
-export function addBar(xPos, barHeight, city) {
-  let YAxisLen = parseInt(chartStyles.height) - parseInt(xPos);
+export function addBar(padding, barHeight, city, spacing) {
+  let YAxisLen = parseInt(chartStyles.height) - parseInt(padding);
   let barStartPoint = YAxisLen - barHeight;
 
   // console.log(barStartPoint, barHeight, YAxisLen);
   let bar = `
-    <rect x="${xPos}" y=${barStartPoint - 1} width="30" height="${barHeight}"/>
-    <text x="${xPos}" y="${xPos + 440}">${city}</text>
+    <rect x="${padding + spacing}" y=${barStartPoint - 1} width="30" height="${barHeight}"/>
+    <text x="${padding + spacing}" y="${padding + 440}">${city}</text>
   `;
   chart.innerHTML += bar;
 }
 
 export async function addBars(padding) {
   const data = await getData();
-
-  let yAxisLen = parseInt(chartStyles.height) - parseInt(padding);
-  let xAxisLen = parseInt(chartStyles.width) - parseInt(padding) * 2;
-
-  let barWidth = 30;
-  let barNum = Object.keys(data).length * barWidth;
-  let currXPos = padding;
-  let totalSpace = xAxisLen - barNum * barWidth;
-  let spaceBetween = totalSpace / (barNum - 1);
   let spacing = 20;
+  let yAxisLen = parseInt(chartStyles.height) - parseInt(padding);
+  let xAxisLen = parseInt(chartStyles.width) - parseInt(padding);
 
   for (const city in data) {
-    addBar(currXPos, Math.abs(yAxisLen * Math.abs(data[city].dMonth)), city);
+    addBar(
+      padding,
+      Math.abs(yAxisLen * Math.abs(data[city].dMonth)),
+      city,
+      spacing,
+    );
 
-    currXPos += barWidth + spaceBetween;
+    spacing += (xAxisLen - padding ) / Object.keys(data).length;
   }
 }
